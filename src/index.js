804aa2a52a87a12d49efad67e925767744857ba5
volha.mashnitskaya <volha.mashnitskaya@gmail.com>
2020-02-23 10:44:03 +0300

    feat: task solution upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length == 0){
+    return 0;
+  }
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length == 0){
+    return 0;
+  }
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length == 0){
+    return 0;
+  }
+  let length = array.length;
+  return (array.reduce((accumulator, value) => accumulator + value, 0)) / length;
 }
+
